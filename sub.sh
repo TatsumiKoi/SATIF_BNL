468d9f7afd5e16532276607ab0f1de7ea61ff3c6
Koi, Tatsumi <tkoi@isc.chubu.ac.jp>
2020-09-08 19:31:18 +0900

    modified:   sub.sh Minor update

--- sub.sh
+++ sub.sh
@@ -3,10 +3,17 @@
 sub_()
 {
    PL=$1
+   TARG=$2
+   ENERGY=$3
    XS=DEFAULT
    #XS=JENDL
    #XS=BARASHENKOV
    #XS=JB
+
+   echo $1 $2 $3
+
+   rm ${PL}_${XS}.*.${ENERGY}.${TARG}.log.gz
+   gzip ${PL}_${XS}.*.${ENERGY}.${TARG}.log 
 
    n=1
    nmax=16
@@ -14,21 +21,21 @@ sub_()
    while [ $n -le $nmax ]
    do
 
-      echo $n
-      rm ${PL}_${XS}.${n}job.*.log.gz
-      gzip ${PL}_${XS}.${n}job.*.log 
+      ./SATIF_BNL ${TARG}.${ENERGY}.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.${ENERGY}.${TARG}.log &
 
-      ./SATIF_BNL Iron.2.83GeV.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.2.83GeV.Iron.log &
-      ./SATIF_BNL Iron.24GeV.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.24GeV.Iron.log &
-      ./SATIF_BNL Concrete.2.83GeV.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.2.83GeV.Concrete.log &
-      ./SATIF_BNL Concrete.24GeV.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.24GeV.Concrete.log &
-
-      wait
       n=`expr $n + 1` 
    done
+   wait
 }
 
-sub_ Shielding
+targ_energy_()
+{
+   PL=$1
+   sub_ $PL Iron 2.83GeV
+   sub_ $PL Iron 24GeV
+   sub_ $PL Concrete 2.83GeV
+   sub_ $PL Concrete 24GeV
+}
+
+targ_energy_ Shielding
 #sub_ QGSP_BERT
-
-
