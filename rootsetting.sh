a2028e2c8e18258e738d688238dd82f93f37e922
KOI, Tatsumi <tkoi@isc.chubu.ac.jp>
2020-09-24 12:05:28 +0900

    Add lines for SCOPE  	modified:   rootsetting.sh

--- rootsetting.sh
+++ rootsetting.sh
@@ -2,7 +2,6 @@
 #export ROOTSYS=/home/tkoi/Simulations/root/6.12.06
 #export ROOTSYS=/opt/root/6.14.06
 export ROOTSYS=/home/tkoi/ChubuU/root/root-6.22.02_install
+#export ROOTSYS=/home/usr4/dc/tkoi/Simulation/Root/root-6.22.02
 export LD_LIBRARY_PATH=$ROOTSYS/lib:${LD_LIBRARY_PATH}
 export PATH=$ROOTSYS/bin:${PATH}
-
-alias vi="vim"
