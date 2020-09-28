40254a38c38331e977c933114d2e680fabea7c7a
KOI, Tatsumi <tkoi@isc.chubu.ac.jp>
2020-09-29 00:15:33 +0900

    Scanning available hosts for submitting jobs  	modified:   rsub.sh

--- rsub.sh
+++ rsub.sh
@@ -17,15 +17,27 @@ sub_()
    n=1
    nmax=16
 
-   if [ $n -lt 10 ]; then 
-      nm=0$n
-   else
-      nm=0$n
-   fi
-
+   hosts=()
+   while [ ${#hosts[*]} -le $nmax ]
+   do 
+      if [ $n -lt 10 ]; then 
+         nm=0$n
+      else
+         nm=$n
+      fi
+      rsh b$nm w > /dev/null
+      if [ $? -eq 0 ]; then
+         hosts+=( $nm )
+      fi 
+      n=`expr $n + 1` 
+   done
+    
+   n=1
    while [ $n -le $nmax ]
    do
       #./SATIF_BNL ${TARG}.${ENERGY}.in ${PL} ${XS} ${n} > ${PL}_${XS}.${n}job.${ENERGY}.${TARG}.log
+      nn=`expr $n - 1`
+      nm=${hosts[$nn]}
       rsh b$nm $PWD/rsubx.sh $PWD SATIF_BNL ${TARG}.${ENERGY}.in ${PL} ${XS} ${n} ${PL}_${XS}.${n}job.${ENERGY}.${TARG}.log 
       n=`expr $n + 1` 
    done
