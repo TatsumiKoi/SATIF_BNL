d294a366613be8d678ec85ef40c228c8fe62f91d
KOI, Tatsumi <tkoi@isc.chubu.ac.jp>
2020-11-25 20:44:52 +0900

    Serching A, B, C zone of SCOPE system at Chubu Univ.  	modified:   check_available_host.sh

--- check_available_host.sh
+++ check_available_host.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-n=0
-nmax=29
-
 rm host 
+
+n=0
+
+#Checking A Zone
+nmax=29
 while [ $n -le $nmax ] 
 do 
    if [ $n -lt 10 ] ; then
@@ -12,8 +14,47 @@ do
       nn=$n
    fi
    la15m=`rsh a${nn}u uptime | cut -d"," -f5`
-   #echo c${nn}u $la15m
-   if [ `echo "$la15m < 1.0" | bc` == 1 ]; then
+   #echo a${nn}u $la15m
+   if [ `echo "$la15m < 1.0" | bc` = 1 ]; then
+      echo a${nn} >> host
+      #echo OK
+   fi
+   n=` expr  $n + 1 ` 
+done
+
+#Checking B Zone
+n=0
+nmax=34
+while [ $n -le $nmax ] 
+do 
+   if [ $n -lt 10 ] ; then
+      nn=0$n
+   else 
+      nn=$n
+   fi
+#   la15m=`rsh b${nn}u uptime | cut -d"," -f5`
+#   #echo a${nn}u $la15m
+#   if [ `echo "$la15m < 1.0" | bc` = 1 ]; then
+#      echo b${nn} >> host
+#      #echo OK
+#   fi
+   n=` expr  $n + 1 ` 
+done
+
+
+#Checking C Zone
+n=0
+nmax=29
+while [ $n -le $nmax ] 
+do 
+   if [ $n -lt 10 ] ; then
+      nn=0$n
+   else 
+      nn=$n
+   fi
+   la15m=`rsh c${nn}u uptime | cut -d"," -f5`
+   #echo a${nn}u $la15m
+   if [ `echo "$la15m < 1.0" | bc` = 1 ]; then
       echo c${nn} >> host
       #echo OK
    fi
